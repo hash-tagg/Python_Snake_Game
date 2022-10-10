7931d14714c04e3160081e13bb6a86e6f541ecbc
Shashank Saxena <69804741+ShashankKumarSaxena@users.noreply.github.com>
2022-10-10 10:33:38 +0530

    Initialize pygame and start with basic

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 import time
 
+# Initialize pygame
+pygame.init()
+
+# Few screen constants
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
 #class Fruit:
 
 #class Snake:
